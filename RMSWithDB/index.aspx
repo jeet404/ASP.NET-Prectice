067fded8d7c2a7adde3b911189179c3807e9009e
Jeet Patel <jeet63240@gmail.com>
2022-07-28 18:30:33 +0530

    Resort booking task completed.

--- RMSWithDB/index.aspx
+++ RMSWithDB/index.aspx
@@ -5,12 +5,55 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Resort Booking System</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
-    
+    <header>
+        <h1>Classic Gir Resorts</h1>
+    </header>
+    <div class="container">
+        <form id="form1" runat="server">
+            <div class="form-control" style="text-align:center;">
+                <h2>Book Your Room</h2>
+                <hr />
+            </div>
+            <div class="form-control">
+                <asp:Label ID="lblfname" runat="server" Text="Full Name"></asp:Label>
+                <asp:TextBox ID="txtFname" runat="server"></asp:TextBox>
+                <asp:Label ID="lblmno" runat="server" Text="Mobile No."></asp:Label>
+                <asp:TextBox ID="txtMno" runat="server"></asp:TextBox>
+            </div>
+            <div class="form-control">
+                <asp:Label ID="Label1" runat="server" Text="Which Room do you want ?"></asp:Label>
+                <asp:DropDownList ID="ddlRooms" runat="server" AutoPostBack="true" AppendDataBoundItems="true" OnSelectedIndexChanged="ddlRooms_SelectedIndexChanged">
+                    <asp:ListItem Value="0" Text="--Select Room--"></asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class="form-control">
+                <asp:Label ID="lblPSec" runat="server" Text="Price :"></asp:Label>
+                <div class="pr-cont">
+                    <asp:Label ID="lblP" runat="server" Text="Price of 1 Person : "></asp:Label>
+                    <asp:Label ID="lblOneP" runat="server" Text="0"></asp:Label>
+                    <asp:TextBox ID="txtPerson" runat="server" TextMode="Number" AutoPostBack="true" OnTextChanged="txtPerson_TextChanged"></asp:TextBox>
+                    <asp:Label ID="lblPTOT" runat="server" Text="0"></asp:Label>
+                    <a></a>
+                </div>
+            </div>
+            <div class="form-control final">
+                <asp:Label ID="lbl1" runat="server" Text="Total Payable Amount"></asp:Label>
+                <hr /><br />
+                <asp:Label ID="lblRMT" runat="server" Text="0"></asp:Label>
+                <asp:Label ID="lbltex" runat="server" Text="+ 18% GST"></asp:Label>
+                <asp:Label ID="lblLine" runat="server" Text="----------------"></asp:Label>
+                <asp:Label ID="lblTOT" runat="server" Text="0"></asp:Label>
+            </div>
+            <div class="form-control btn-cont">
+                <asp:Button ID="btnCal" runat="server" Text="Book Now" OnClick="btnCal_Click" />
+            </div>
+            <div class="form-control">
+                <asp:Label ID="lblres" runat="server"></asp:Label>
+            </div>
+        </form>
     </div>
-    </form>
 </body>
 </html>
--- RMSWithDB/index.aspx.cs
+++ RMSWithDB/index.aspx.cs
@@ -4,11 +4,120 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 public partial class index : System.Web.UI.Page
 {
+    SqlConnection conn;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataSet ds;
+    string strCon = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\WorkSpace\aspPrecticle\RMSWithDB\App_Data\rooms.mdf;Integrated Security=True";
+    int person = 2;
+    int app = 0;
+    int roomcost = 0;
+    int totofper = 0;
+    int totbill = 0;
+    int nettot = 0;
+    int tex = 18;
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            conn = new SqlConnection(strCon);
+            conn.Open();
+            if (!IsPostBack)
+            {
+                getRooms();
+            }
+        }
+        catch
+        {
+            Response.Write(e);
+        }
+    }
 
+    public void getRooms()
+    {
+        string getroom = "SELECT * FROM rooms";
+        da = new SqlDataAdapter(getroom, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        ddlRooms.DataSource = ds;
+        ddlRooms.DataValueField = "r_type";
+        ddlRooms.DataTextField = "r_type";
+        ddlRooms.DataBind();
+    }
+    protected void ddlRooms_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string getroom = "SELECT * FROM rooms WHERE r_type = '"+ddlRooms.SelectedValue+"'";
+        da = new SqlDataAdapter(getroom, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        if (ddlRooms.SelectedValue != "0")
+        {
+            app = Convert.ToInt32(ds.Tables[0].Rows[0][2]);
+            totofper = app * person;
+            totbill = totofper;
+            nettot = totbill + ((totbill * tex) / 100);
+        }
+        else
+        {
+            app = 0;
+            totofper = 0;
+            totbill = totofper;
+            nettot = 0;
+        }
+        lblOneP.Text = app.ToString();
+        txtPerson.Text = person.ToString();
+        lblPTOT.Text = totofper.ToString();
+        lblRMT.Text = totbill.ToString();
+        lblTOT.Text = nettot.ToString();
+    }
+    protected void txtPerson_TextChanged(object sender, EventArgs e)
+    {
+        string getroom = "SELECT r_price FROM rooms WHERE r_type = '" + ddlRooms.SelectedValue + "'";
+        da = new SqlDataAdapter(getroom, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        if (txtPerson.Text != 0.ToString())
+        {
+            app = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+            person =  Convert.ToInt32(txtPerson.Text);
+            totofper = app * person;
+            totbill = totofper;
+        }
+        nettot = totbill + ((totbill * tex) / 100);
+        txtPerson.Text = person.ToString();
+        lblPTOT.Text = totofper.ToString();
+        lblRMT.Text = totbill.ToString();
+        lblTOT.Text = nettot.ToString();
+    }
+    protected void btnCal_Click(object sender, EventArgs e)
+    {
+        string strIn = "INSERT INTO bookings VALUES(@name,@roomtype,@totper,@totbill)";
+        cmd = new SqlCommand(strIn, conn);
+        cmd.Parameters.AddWithValue("@name", txtFname.Text);
+        cmd.Parameters.AddWithValue("@roomtype", ddlRooms.SelectedValue);
+        cmd.Parameters.AddWithValue("@totper", txtPerson.Text);
+        cmd.Parameters.AddWithValue("@totbill", lblTOT.Text);
+        int res = cmd.ExecuteNonQuery();
+        if (res > 0)
+        {
+            lblres.Text = "Room Booked Successfully";
+        }
+        else
+        {
+            lblres.Text = "Issue in booking!";
+        }
+        txtFname.Text = "";
+        txtMno.Text = "";
+        ddlRooms.SelectedValue = "0";
+        lblOneP.Text = app.ToString();
+        txtPerson.Text = person.ToString();
+        lblPTOT.Text = totofper.ToString();
+        lblRMT.Text = totbill.ToString();
+        lblTOT.Text = nettot.ToString();
     }
 }
