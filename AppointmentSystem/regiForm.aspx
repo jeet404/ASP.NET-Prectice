641dca6d3811c530223ddf332d361f01b7c07b60
Jeet Patel <jeet63240@gmail.com>
2022-08-07 10:31:07 +0530

    Registration done.

---
+++ AppointmentSystem/regiForm.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="regiForm.aspx.cs" Inherits="regiForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Register Appointment</title>
+    <style>
+        div{
+            margin-bottom:16px;
+            display:flex;
+            justify-content:space-between;
+        }
+    </style>
+</head>
+<body>
+     <div style="display:flex;justify-content:center;">
+        <form id="form1" runat="server">
+                <h2>Registration Form</h2>
+                <div>
+                    <asp:Label ID="lbl1" runat="server" Text="Full Name : "></asp:Label>
+                    <asp:TextBox ID="txtfname" runat="server"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="lbl2" runat="server" Text="Email : "></asp:Label>
+                    <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="lbl4" runat="server" Text="Mobile No : "></asp:Label>
+                    <asp:TextBox ID="txtmobile" runat="server"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="lbl7" runat="server" Text="Gender : "></asp:Label>
+                    <asp:RadioButton ID="rdoM" runat="server" Text="Male" GroupName="gen" />
+                    <asp:RadioButton ID="rdoF" runat="server" Text="Female" GroupName="gen" />
+                    <asp:RadioButton ID="rdoO" runat="server" Text="Other" GroupName="gen" />
+                </div>
+                <div>
+                    <asp:Label ID="Label1" runat="server" Text="Date of Birth : "></asp:Label>
+                    <asp:TextBox ID="txtDob" runat="server" TextMode="Date"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="lbl8" runat="server" Text="Choose Person : "></asp:Label>
+                    <asp:DropDownList ID="ddlChoice" runat="server" AutoPostBack="true" AppendDataBoundItems="true">
+                        <asp:ListItem Text="--Select Person--" Value="0"></asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                <div>
+                    <asp:Label ID="lbl9" runat="server" Text="Choose Date : "></asp:Label>
+                    <asp:TextBox ID="txtSlot" runat="server" TextMode="Date"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Button ID="btnRegi" runat="server" Text="Done" OnClick="btnRegi_Click" />
+                    <br /><br />
+                    <asp:Button ID="btnLogin" runat="server" Text="View Appointment" OnClick="btnLogin_Click" />
+                </div>
+                <div>
+                    <asp:Label ID="lblMsg" runat="server"></asp:Label>
+                </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ AppointmentSystem/regiForm.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class regiForm : System.Web.UI.Page
+{
+    SqlConnection conn;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataSet ds;
+    string gen;
+    int apId;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string strCon = ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString;
+        try
+        {
+            conn = new SqlConnection(strCon);
+            conn.Open();
+            if (!IsPostBack)
+            {
+                getExperts();
+            }
+        }
+        catch(SqlException se)
+        {
+            Response.Write(se);
+        }
+    }
+
+    public void getExperts()
+    {
+        string strExp = "SELECT * FROM experts";
+        da = new SqlDataAdapter(strExp, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        ddlChoice.DataSource = ds;
+        ddlChoice.DataValueField = "e_name";
+        ddlChoice.DataTextField = "e_name";
+        ddlChoice.DataBind();
+    }
+
+    public void chkGen()
+    {
+        if (rdoM.Checked)
+        {
+            gen = rdoM.Text;
+        }
+        else if (rdoF.Checked)
+        {
+            gen = rdoF.Text;
+        }
+        else if (rdoO.Checked)
+        {
+            gen = rdoO.Text;
+        }
+    }
+
+    public void GenId()
+    {
+        apId= new Random().Next(99999,500000);
+        //Response.Write(apId);
+    }
+
+    public void clrForm()
+    {
+        txtfname.Text = "";
+        txtemail.Text = "";
+        txtmobile.Text = "";
+        txtDob.Text = "";
+        txtSlot.Text = "";
+        ddlChoice.SelectedValue = "0";
+        rdoM.Checked = false;
+        rdoF.Checked = false;
+        rdoO.Checked = false;
+    }
+
+    protected void btnRegi_Click(object sender, EventArgs e)
+    {
+        GenId();
+        chkGen();
+        string strIn = "INSERT INTO appointment VALUES(@id,@name,@email,@mobile,@gender,@dob,@expert,@slot)";
+        cmd = new SqlCommand(strIn,conn);
+        cmd.Parameters.AddWithValue("@id",apId);
+        cmd.Parameters.AddWithValue("@name",txtfname.Text);
+        cmd.Parameters.AddWithValue("@email",txtemail.Text);
+        cmd.Parameters.AddWithValue("@mobile",txtmobile.Text);
+        cmd.Parameters.AddWithValue("@gender",gen);
+        cmd.Parameters.AddWithValue("@dob",txtDob.Text);
+        cmd.Parameters.AddWithValue("@expert",ddlChoice.SelectedValue);
+        cmd.Parameters.AddWithValue("@slot",txtSlot.Text);
+        int res = cmd.ExecuteNonQuery();
+        if(res > 0){
+            lblMsg.ForeColor = System.Drawing.Color.Green;
+            lblMsg.Text = "Your Appointment ID :  " + apId.ToString();
+            clrForm();
+        }
+        else
+        {
+            lblMsg.ForeColor = System.Drawing.Color.Red;
+            lblMsg.Text = "There are some Problem!";
+        }
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ViewAp.aspx");
+    }
+}
