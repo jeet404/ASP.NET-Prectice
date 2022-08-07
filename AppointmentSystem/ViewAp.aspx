222b2bcd3fc267761cb0958fb43af4dfc9ba843b
Jeet Patel <jeet63240@gmail.com>
2022-08-07 11:13:35 +0530

    Verification done.

---
+++ AppointmentSystem/ViewAp.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ViewAp.aspx.cs" Inherits="ViewAp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>View Appointment</title>
+    <style>
+        div{
+            margin-bottom:16px;
+            display:flex;
+            justify-content:space-between;
+        }
+    </style>
+</head>
+<body>
+    <div style="display:flex;justify-content:center;">
+        <form id="form1" runat="server">
+            <h1>View Your Appointment</h1>
+            <div>
+                <asp:Label ID="lbl1" runat="server" Text="Appointment Id : "></asp:Label>
+                <asp:TextBox ID="txtApId" runat="server" OnTextChanged="txtApId_TextChanged" AutoPostBack="true"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Label ID="lblMsg" runat="server"></asp:Label>
+            </div>
+            <div>
+                <asp:Label ID="lbl2" runat="server" Text="Select Date of Birth : "></asp:Label>
+                <asp:TextBox ID="txtDob" runat="server" TextMode="Date"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Button ID="btnView" runat="server" Text="View" OnClick="btnView_Click" />
+            </div>
+            <div>
+                <asp:Label ID="lblErr" runat="server" ForeColor="Red"></asp:Label>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ AppointmentSystem/ViewAp.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class ViewAp : System.Web.UI.Page
+{
+    SqlConnection conn;
+    SqlDataAdapter da;
+    DataSet ds;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string strCon = ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString;
+        try
+        {
+            conn = new SqlConnection(strCon);
+            conn.Open();
+            txtDob.Enabled = false;
+            btnView.Enabled = false;
+        }
+        catch (SqlException se)
+        {
+            Response.Write(se);
+        }
+    }
+    protected void btnView_Click(object sender, EventArgs e)
+    {
+        string strGet = "SELECT CONVERT(VARCHAR,ap_dob,23) FROM appointment WHERE ap_id = " + txtApId.Text;
+        da = new SqlDataAdapter(strGet, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        if (txtDob.Text == ds.Tables[0].Rows[0][0].ToString())
+        {
+            Session["apid"] = txtApId.Text;
+            Response.Redirect("index.aspx");
+        }
+        else
+        {
+            lblErr.Text = "There are Some Issue!";
+        }
+    }
+    protected void txtApId_TextChanged(object sender, EventArgs e)
+    {
+        if (txtApId.Text != "")
+        {
+            string chk = "SELECT * FROM appointment WHERE ap_id = " + txtApId.Text;
+            da = new SqlDataAdapter(chk, conn);
+            ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                txtDob.Enabled = true;
+                btnView.Enabled = true;
+                lblMsg.ForeColor = System.Drawing.Color.Green;
+                lblMsg.Text = "You are Good to Go";
+            }
+            else
+            {
+                txtDob.Enabled = false;
+                btnView.Enabled = false;
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+                lblMsg.Text = "Your Appointment Id is Invalid!";
+            }
+        }
+        else
+        {
+            lblMsg.Text = "";
+        }
+    }
+}
