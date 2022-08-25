7b54c23bdefa37226fc03302ab0247b2bee24fa7
Jeet Patel <jeet63240@gmail.com>
2022-08-25 15:02:20 +0530

    Category Module Done.

---
+++ MiniProjectTask/category.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="category.aspx.cs" Inherits="category" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div class="container">
+        <h1>Category</h1>
+        <hr />
+        <div class="row">
+            <div class="form-style">
+                <div class="form-control">
+                    <asp:Label ID="lblcname" runat="server" Text="Category Name : "></asp:Label>
+                    <asp:TextBox ID="txtCateName" runat="server"></asp:TextBox>
+                </div>
+                <div class="form-control">
+                    <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+                </div>
+                <div class="form-control">
+                    <asp:Label ID="lblErr" runat="server" ForeColor="Red"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <asp:GridView ID="gvCategory" runat="server" PageSize="5" DataKeyNames="c_id" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="false" Width="300px" CellPadding="4" ForeColor="#333333" GridLines="None" OnPageIndexChanging="gvCategory_PageIndexChanging" OnRowCancelingEdit="gvCategory_RowCancelingEdit" OnRowDeleting="gvCategory_RowDeleting" OnRowEditing="gvCategory_RowEditing" OnRowUpdating="gvCategory_RowUpdating">
+                <Columns>
+                    <asp:BoundField HeaderText="Category Name" DataField="c_name" />
+                    <asp:CommandField ShowEditButton="True" />
+                    <asp:CommandField ShowDeleteButton="True" />
+                </Columns>
+                <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                <EditRowStyle BackColor="#999999" />
+                <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ MiniProjectTask/category.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class category : System.Web.UI.Page
+{
+    SqlConnection conn;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataSet ds;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (Session["email"] != null)
+        //{
+        try
+        {
+            string strCon = ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString;
+            conn = new SqlConnection(strCon);
+            conn.Open();
+            if (!IsPostBack)
+            {
+                bindCat();
+            }
+        }
+        catch (SqlException se)
+        {
+            Response.Write(se);
+        }
+        //}
+        //else
+        //{
+        //    Response.Redirect("login.aspx");
+        //}
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        string strGet = "SELECT * FROM categories";
+        da = new SqlDataAdapter(strGet, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        string cname = ds.Tables[0].Rows[0][1].ToString();
+        if (cname != txtCateName.Text)
+        {
+            string strIn = "INSERT INTO categories VALUES (@name)";
+            cmd = new SqlCommand(strIn, conn);
+            cmd.Parameters.Add("@name", txtCateName.Text);
+            int res = cmd.ExecuteNonQuery();
+            if (res > 0)
+            {
+                bindCat();
+            }
+            txtCateName.Text = "";
+        }
+        else
+        {
+            lblErr.Text = "Already Exsists!";
+        }
+    }
+
+    public void bindCat()
+    {
+        string strGet = "SELECT * FROM categories";
+        da = new SqlDataAdapter(strGet, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        gvCategory.DataSource = ds;
+        gvCategory.DataBind();
+        lblErr.Text = "";
+    }
+    protected void gvCategory_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        gvCategory.EditIndex = e.NewEditIndex;
+        this.bindCat();
+    }
+    protected void gvCategory_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        int cid = Convert.ToInt32(gvCategory.DataKeys[e.RowIndex].Values[0]);
+        string strDel = "DELETE FROM categories WHERE c_id = " + cid;
+        cmd = new SqlCommand(strDel, conn);
+        int res = cmd.ExecuteNonQuery();
+        if (res > 0)
+        {
+            this.bindCat();
+        }
+    }
+    protected void gvCategory_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        int cid = Convert.ToInt32(gvCategory.DataKeys[e.RowIndex].Values[0]);
+        string strEdit = "UPDATE categories SET c_name=@name WHERE c_id = " + cid;
+        GridViewRow gvr = (GridViewRow)gvCategory.Rows[e.RowIndex];
+        TextBox cname = (TextBox)gvr.Cells[0].Controls[0];
+        cmd = new SqlCommand(strEdit, conn);
+        cmd.Parameters.Add("@name", cname.Text);
+        int res = cmd.ExecuteNonQuery();
+        if (res > 0)
+        {
+            bindCat();
+        }
+    }
+    protected void gvCategory_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvCategory.PageIndex = e.NewPageIndex;
+        this.bindCat();
+    }
+    protected void gvCategory_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        gvCategory.EditIndex = -1;
+        this.bindCat();
+    }
+}
