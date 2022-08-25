27b541a74fbd12939f5ab043da65d70474973410
Jeet Patel <jeet63240@gmail.com>
2022-08-25 13:47:39 +0530

    Put login.

---
+++ MiniProjectTask/login.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+    <style>
+        div{
+            display:flex;
+            justify-content:space-between;
+        }
+    </style>
+</head>
+<body>
+    <div style="display:flex;justify-content:center;margin-top:20px;">
+        <form id="form1" runat="server">
+            <h1>Login</h1>
+                <div>
+                    <asp:Label ID="Label1" runat="server" Text="Email : "></asp:Label>
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                </div>
+                <br />
+                <div>
+                    <asp:Label ID="Label2" runat="server" Text="Password : "></asp:Label>
+                    <asp:TextBox ID="txtPass" runat="server" TextMode="Password"></asp:TextBox>
+                </div>
+                <br /><br />
+                <div>
+                    <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+                </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ MiniProjectTask/login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class login : System.Web.UI.Page
+{
+    SqlConnection conn;
+    SqlDataAdapter da;
+    DataSet ds;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            string strCon = ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString;
+            conn = new SqlConnection(strCon);
+            conn.Open();
+        }
+        catch (SqlException se)
+        {
+            Response.Write(se);
+        }
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        string strGet = "SELECT * FROM admins WHERE email = '" + txtEmail.Text + "' AND password = '" + txtPass.Text + "'";
+        da = new SqlDataAdapter(strGet, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            Session["email"] = txtEmail.Text;
+            Response.Redirect("index.aspx");
+        }
+        else
+        {
+            Response.Write("Wrong Email or Password!");
+        }
+    }
+}
