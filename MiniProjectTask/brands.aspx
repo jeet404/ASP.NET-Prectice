a609f37f14926b9768be8dcd9012ec5c2bb16714
Jeet Patel <jeet63240@gmail.com>
2022-08-25 15:23:31 +0530

    Brand Module Done.

---
+++ MiniProjectTask/brands.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="brands.aspx.cs" Inherits="brands" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div class="container">
+        <h1>Brands</h1>
+        <hr />
+        <div class="row">
+            <div class="form-style">
+                <div class="form-control">
+                    <asp:Label ID="lblcname" runat="server" Text="Brand Name : "></asp:Label>
+                    <asp:TextBox ID="txtBrandName" runat="server"></asp:TextBox>
+                </div>
+                <div class="form-control">
+                    <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+                </div>
+                <div class="form-control">
+                    <asp:Label ID="lblErr" runat="server" ForeColor="Red"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <asp:GridView ID="gvBrands" runat="server" PageSize="5" DataKeyNames="b_id" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="false" Width="300px" CellPadding="4" ForeColor="#333333" GridLines="None" OnPageIndexChanging="gvBrands_PageIndexChanging" OnRowCancelingEdit="gvBrands_RowCancelingEdit" OnRowDeleting="gvBrands_RowDeleting" OnRowEditing="gvBrands_RowEditing" OnRowUpdating="gvBrands_RowUpdating">
+                <Columns>
+                    <asp:BoundField HeaderText="Brand Name" DataField="b_name" />
+                    <asp:CommandField ShowEditButton="True" />
+                    <asp:CommandField ShowDeleteButton="True" />
+                </Columns>
+                <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                <EditRowStyle BackColor="#999999" />
+                <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ MiniProjectTask/brands.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class brands : System.Web.UI.Page
+{
+    SqlConnection conn;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataSet ds;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (Session["email"] != null)
+        //{
+        try
+        {
+            string strCon = ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString;
+            conn = new SqlConnection(strCon);
+            conn.Open();
+            if (!IsPostBack)
+            {
+                bindBrand();
+            }
+        }
+        catch (SqlException se)
+        {
+            Response.Write(se);
+        }
+        //}
+        //else
+        //{
+        //    Response.Redirect("login.aspx");
+        //}
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        string strGet = "SELECT * FROM brands";
+        da = new SqlDataAdapter(strGet, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        string cname = ds.Tables[0].Rows[0][1].ToString();
+        if (cname != txtBrandName.Text)
+        {
+            string strIn = "INSERT INTO brands VALUES (@name)";
+            cmd = new SqlCommand(strIn, conn);
+            cmd.Parameters.AddWithValue("@name", txtBrandName.Text);
+            int res = cmd.ExecuteNonQuery();
+            if (res > 0)
+            {
+                bindBrand();
+            }
+            txtBrandName.Text = "";
+        }
+        else
+        {
+            lblErr.Text = "Already Exsists!";
+        }
+    }
+
+    public void bindBrand()
+    {
+        string strGet = "SELECT * FROM brands";
+        da = new SqlDataAdapter(strGet, conn);
+        ds = new DataSet();
+        da.Fill(ds);
+        gvBrands.DataSource = ds;
+        gvBrands.DataBind();
+        lblErr.Text = "";
+    }
+    protected void gvBrands_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvBrands.PageIndex = e.NewPageIndex;
+        this.bindBrand();
+    }
+    protected void gvBrands_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        gvBrands.EditIndex = -1;
+        this.bindBrand();
+    }
+    protected void gvBrands_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        int cid = Convert.ToInt32(gvBrands.DataKeys[e.RowIndex].Values[0]);
+        string strDel = "DELETE FROM brands WHERE b_id = " + cid;
+        cmd = new SqlCommand(strDel, conn);
+        int res = cmd.ExecuteNonQuery();
+        if (res > 0)
+        {
+            this.bindBrand();
+        }
+    }
+    protected void gvBrands_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        gvBrands.EditIndex = e.NewEditIndex;
+        this.bindBrand();
+    }
+    protected void gvBrands_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        int cid = Convert.ToInt32(gvBrands.DataKeys[e.RowIndex].Values[0]);
+        string strEdit = "UPDATE brands SET b_name=@name WHERE b_id = " + cid;
+        GridViewRow gvr = (GridViewRow)gvBrands.Rows[e.RowIndex];
+        TextBox bname = (TextBox)gvr.Cells[0].Controls[0];
+        cmd = new SqlCommand(strEdit, conn);
+        cmd.Parameters.AddWithValue("@name", bname.Text);
+        int res = cmd.ExecuteNonQuery();
+        if (res > 0)
+        {
+            bindBrand();
+        }
+    }
+}
